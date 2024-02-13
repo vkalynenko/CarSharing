698e2e35adf61805472901d719fa44762debf77a
Valeriia <Kalynenko>
2024-02-13 20:22:22 +0200

    Added main BE infrastructure

---
+++ CarSharing/Extensions/DependencyInjectionConfig.cs
@@ -0,0 +1,31 @@
+﻿namespace CarSharing.Extensions
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection RegisterDependencies(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            RegisterServices(services);
+            RegisterDataAccess(services, configuration);
+            RegisterExceptionHandlers(services);
+
+            return services;
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            
+        }
+
+        private static void RegisterDataAccess(this IServiceCollection services, IConfiguration configuration)
+        {
+           
+        }
+
+        private static void RegisterExceptionHandlers(this IServiceCollection services)
+        {
+           
+        }
+    }
+}
+}
---
+++ CarSharing/Program.cs
@@ -0,0 +1,29 @@
+using CarSharing.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+builder.Services.RegisterDependencies(configuration);
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
