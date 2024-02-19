1f8fe38c0074fb1b4f61c94832411460b2e3e4a0
Valeriia <Kalynenko>
2024-02-19 02:16:17 +0200

    Clients Frontend

--- CarSharing.FrontEnd/src/app/services/car.service.ts
+++ CarSharing.FrontEnd/src/app/services/car.service.ts
@@ -11,39 +11,7 @@ export class CarService {
     constructor(private http: HttpClient) {}
     
     public getAllCars(): Observable<Car[]> {
-        const fakeCars: Car[] = [
-            { 
-                id: 1,
-                brand: 'Toyota', 
-                model: 'Camry', 
-                releaseYear: 2020, 
-                gearBox: 'Automatic', 
-                seatsQuantity: 5, 
-                dailyRentalPrice: 50, 
-                isInUse: true
-            },
-            { 
-                id: 2,
-                brand: 'Honda', 
-                model: 'Civic', 
-                releaseYear: 2019, 
-                gearBox: 'Manual', 
-                seatsQuantity: 5, 
-                dailyRentalPrice: 45, 
-            },
-            { 
-                id: 3,
-                brand: 'Ford', 
-                model: 'Fusion', 
-                releaseYear: 2018, 
-                gearBox: 'Automatic', 
-                seatsQuantity: 5,
-                dailyRentalPrice: 48, 
-            }
-            // Add more car models as needed
-        ];
-        return of(fakeCars);
-       // return this.http.get<Car[]>(`${environments.apiUrl}/Car`);
+       return this.http.get<Car[]>(`${environments.apiUrl}/car`);
     }
 
     public getCarById(id: number): Observable<Car> {
